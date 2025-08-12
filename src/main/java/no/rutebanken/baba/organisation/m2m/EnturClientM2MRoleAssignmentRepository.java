3e6de3e1a37ac2ef331a326e8c9a8f50abb940cf
Vincent Paturet <vincent.paturet@entur.org>
2025-08-12 15:27:01 +0200

    Remove configuration for Entur M2M clients.

--- src/main/java/no/rutebanken/baba/organisation/m2m/EnturClientM2MRoleAssignmentRepository.java
+++ src/main/java/no/rutebanken/baba/organisation/m2m/EnturClientM2MRoleAssignmentRepository.java
@@ -1,4 +1,4 @@
-package no.rutebanken.baba.organisation.m2m.support;
+package no.rutebanken.baba.organisation.m2m;
 
 import no.rutebanken.baba.organisation.model.responsibility.ResponsibilitySet;
 import no.rutebanken.baba.organisation.model.user.M2MClient;
@@ -6,29 +6,61 @@ import no.rutebanken.baba.organisation.repository.M2MClientRepository;
 import no.rutebanken.baba.organisation.util.RoleAssignmentMapper;
 import org.entur.ror.permission.AuthenticatedUser;
 import org.rutebanken.helper.organisation.RoleAssignment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class M2MUtils {
 
-    private M2MUtils() {}
+/**
+ * Build role assignments for Entur Client machine-to-machine tokens.
+ * Role assignments are built from the Baba database.
+ * TODO Permission Store migration
+ */
+public class EnturClientM2MRoleAssignmentRepository {
 
-    public static final String CLIENT_SUBJECT_SUFFIX = "@clients";
+    static final String CLIENT_SUBJECT_SUFFIX = "@clients";
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EnturClientM2MRoleAssignmentRepository.class);
+
+    private final M2MClientRepository repository;
+
+    public EnturClientM2MRoleAssignmentRepository(M2MClientRepository repository) {
+        this.repository = repository;
+    }
+
+    /**
+     * Extract RoleAssignments from the baba database.
+     */
+    public List<RoleAssignment> getRolesAssignments(AuthenticatedUser authenticatedUser) {
+        validateM2MClient(authenticatedUser);
+        List<RoleAssignment> rolesFromDatabase = getRoleAssignmentsFromDatabase(authenticatedUser, repository);
+        LOGGER.info("Returning {} role assignments from database for client {} and organisation {}",
+                rolesFromDatabase.size(), authenticatedUser.subject(), authenticatedUser.organisationId());
+        return rolesFromDatabase;
+    }
+
+    public String getClientName(AuthenticatedUser authenticatedUser) {
+        String clientId = clientId(authenticatedUser.subject());
+        M2MClient client = repository.getOneByPublicId(clientId);
+        return client.getName();
+
+    }
 
     /**
      * Extract the OAuth2 client-id from the subject claim.
      * The subject claim structure for m2m clients is "client-id@clients"
      */
-    public static String clientId(String subject) {
+    private static String clientId(String subject) {
         return subject.replace(CLIENT_SUBJECT_SUFFIX, "");
     }
 
     /**
      * Extract the role assignments from a given M2M client.
      */
-    public static Stream<RoleAssignment> toRoleAssignments(M2MClient client) {
+    private static Stream<RoleAssignment> toRoleAssignments(M2MClient client) {
         return client.getResponsibilitySets().stream()
                 .map(ResponsibilitySet::getRoles)
                 .flatMap(Collection::stream)
@@ -37,14 +69,14 @@ public class M2MUtils {
 
 
 
-    public static void validateM2MClient(AuthenticatedUser authenticatedUser) {
+    private static void validateM2MClient(AuthenticatedUser authenticatedUser) {
         if (!authenticatedUser.isClient()) {
             throw new IllegalArgumentException("The user is not a machine-to-machine client: " + authenticatedUser.subject());
         }
     }
 
 
-    public static List<RoleAssignment> getRoleAssignmentsFromDatabase(AuthenticatedUser authenticatedUser, M2MClientRepository repository) {
+    private static List<RoleAssignment> getRoleAssignmentsFromDatabase(AuthenticatedUser authenticatedUser, M2MClientRepository repository) {
         M2MClient client = repository.getOneByPublicIdIfExists(clientId(authenticatedUser.subject()));
         if (client == null) {
             return List.of();
