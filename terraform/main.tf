b17a6549379c850b4db303f1ba99c8efc088ba50
Vincent Paturet <vincent.paturet@entur.org>
2025-12-05 12:25:09 +0100

    Remove Terraform Kubernetes plugin

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,15 +6,8 @@ terraform {
       source = "hashicorp/google"
       version = "~> 6.49.0"
     }
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = "~> 2.38.0"
-    }
   }
 }
-
-provider "google" {}
-provider "kubernetes" {}
 
 resource "google_sql_database_instance" "db_instance" {
   name             = "baba-db-pg13"
